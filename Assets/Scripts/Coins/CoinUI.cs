624a9dde71c044f8a4e40c89c6f2761e2d799483
MiguelVDP <miguelvidaldelaplaza@gmail.com>
2021-12-28 20:05:52 +0100

    Ahora hay contador de puntos y el puntos dobles funciona

---
+++ Assets/Scripts/Coins/CoinUI.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+
+namespace coins
+{
+    public class CoinUI : MonoBehaviour
+    {
+        public TextMeshProUGUI coinText;
+        private void Awake()
+        {
+            CoinManager.Instance.coinsChanged += HandleCoins;
+        }
+
+        private void HandleCoins(object sender, int e)
+        {
+            coinText.text = e.ToString();
+        }
+    }
+}
--- Assets/Scripts/PowerUps/Types/PointsX2.cs
+++ Assets/Scripts/PowerUps/Types/PointsX2.cs
@@ -1,7 +1,17 @@
-﻿namespace PowerUps.Types
+﻿using System;
+using UnityEngine;
+
+namespace PowerUps.Types
 {
     public class PointsX2 : PowerUp
     {
-        
+        public delegate void ptsX2Event();
+
+        public static event ptsX2Event ptsx2Picked; 
+        private void OnCollisionEnter(Collision other)
+        {
+            ptsx2Picked?.Invoke();
+            Destroy(gameObject);
+        }
     }
 }
