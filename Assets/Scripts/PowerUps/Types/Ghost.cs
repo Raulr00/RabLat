5d3dedfad5e8dbb92ee3d3fa5a2cf50eced368e9
MiguelVDP <miguelvidaldelaplaza@gmail.com>
2021-12-28 21:05:29 +0100

    Spawn de powerUps funcional

--- Assets/Scripts/PowerUps/Types/Ghost.cs
+++ Assets/Scripts/PowerUps/Types/Ghost.cs
@@ -1,7 +1,14 @@
-﻿namespace PowerUps.Types
+﻿using System;
+using UnityEngine;
+
+namespace PowerUps.Types
 {
     public class Ghost : PowerUp
     {
-        
+        private void OnCollisionEnter(Collision other)
+        {
+            Debug.Log("Ghost mode on");
+            Destroy(gameObject);
+        }
     }
 }
