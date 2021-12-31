d81ec4f92c89a23beb00b33373c6f41ea3f9829e
MiguelVDP <miguelvidaldelaplaza@gmail.com>
2021-12-31 18:07:48 +0100

    Ghost ahora funciona de locos

--- Assets/Scripts/PowerUps/Types/Ghost.cs
+++ Assets/Scripts/PowerUps/Types/Ghost.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Net.NetworkInformation;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 namespace PowerUps.Types
@@ -10,15 +11,42 @@ namespace PowerUps.Types
         public float duration = 10;
         private void OnCollisionEnter(Collision other)
         {
-            other.gameObject.layer = 10;
-            Destroy(gameObject);
+            if(!other.gameObject.name.Equals("player")) return;
+            SetLayerRecusively(other.gameObject, 10);
             StartCoroutine(ReturnToNormal(other.gameObject));
+            HidePowerUp();
         }
-
+        
         private IEnumerator ReturnToNormal(GameObject player)
         {
+            Debug.Log("Ghost mode on");
             yield return new WaitForSeconds(duration);
-            player.layer = 7;
+            Debug.Log("Se comienza a cambiar la layer otra vez");
+            SetLayerRecusively(player, 7);
+            Debug.Log("Fin del ghost");
+            Destroy(gameObject);
         }
+
+        private void HidePowerUp()
+        {
+            GetComponent<Collider>().enabled = false;
+            foreach (Transform child in gameObject.transform)
+            {
+                child.gameObject.SetActive(false);
+            }
+        }
+
+        private void SetLayerRecusively(GameObject obj, int newLayer)
+        {
+            if(obj == null) return;
+            obj.layer = newLayer;
+            foreach (Transform child in obj.transform)
+            {
+                if(child == null) continue;
+                SetLayerRecusively(child.gameObject, newLayer);
+            }
+        }
+
+       
     }
 }
