aa43972e16a76e18febbb565007b93c770ac8b76
MiguelVDP <miguelvidaldelaplaza@gmail.com>
2021-12-31 14:44:08 +0100

    PowerUpGhost funcionando

--- Assets/Scripts/PowerUps/Types/Ghost.cs
+++ Assets/Scripts/PowerUps/Types/Ghost.cs
@@ -1,14 +1,24 @@
 ﻿using System;
+using System.Collections;
+using System.Net.NetworkInformation;
 using UnityEngine;
 
 namespace PowerUps.Types
 {
     public class Ghost : PowerUp
     {
+        public float duration = 10;
         private void OnCollisionEnter(Collision other)
         {
-            Debug.Log("Ghost mode on");
+            other.gameObject.layer = 10;
             Destroy(gameObject);
+            StartCoroutine(ReturnToNormal(other.gameObject));
+        }
+
+        private IEnumerator ReturnToNormal(GameObject player)
+        {
+            yield return new WaitForSeconds(duration);
+            player.layer = 7;
         }
     }
 }
