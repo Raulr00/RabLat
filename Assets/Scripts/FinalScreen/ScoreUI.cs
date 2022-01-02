254b9a8a4fbf281ce31724c82b5c616f8cea3754
MiguelVDP <miguelvidaldelaplaza@gmail.com>
2022-01-02 18:52:17 +0100

    HighScore implementado

---
+++ Assets/Scripts/FinalScreen/ScoreUI.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class ScoreUI : MonoBehaviour
+{
+    [SerializeField]public TextMeshProUGUI scoreTxt;
+    [SerializeField]public TextMeshProUGUI highScoreTxt;
+    private void OnEnable()
+    {
+        int score = PlayerPrefs.GetInt("Score");
+        int highScore = PlayerPrefs.GetInt("HighScore", -1);
+        if (score > highScore)
+        {
+            PlayerPrefs.SetInt("HighScore", score);
+            highScore = score;
+        }
+
+        scoreTxt.text = score.ToString();
+        highScoreTxt.text = highScore.ToString();
+
+    }
+}
