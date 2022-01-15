f484b40bce4f738f373842eb7da4bb10d562d003
MiguelVDP <miguelvidaldelaplaza@gmail.com>
2022-01-15 13:54:07 +0100

    HotFix
    Se ha apañado el tema de los sliders

---
+++ Assets/Scripts/menus/SlidersUI.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace menus
+{
+    public class SlidersUI : MonoBehaviour
+    {
+        public Slider sliderMusic;
+        public Slider sliderSFX;
+        public Toggle tick;
+
+        private void OnEnable()
+        {
+            float musicVol = PlayerPrefs.GetFloat("MusicVol", 0.08f);
+            float sfxVol = PlayerPrefs.GetFloat("SFXVol", 0.25f);
+
+            MusicManager.Instance._audioSource.volume = musicVol;
+            SFXManager.Instance.audioSource.volume = sfxVol;
+
+            sliderMusic.value = musicVol;
+            sliderSFX.value = sfxVol;
+
+            sliderMusic.onValueChanged.AddListener(delegate { SetMusicVolume(sliderMusic.value); });
+            sliderSFX.onValueChanged.AddListener(delegate { SetSFXVolume(sliderSFX.value); });
+
+            if (!tick) return;
+            
+            if (PlayerPrefs.GetString("tuto", "True").Equals("True"))
+            {
+                tick.isOn = true;
+            }
+            else
+            {
+                tick.isOn = false;
+            }
+            
+            tick.onValueChanged.AddListener(delegate { setTutorial(tick.isOn); });
+        }
+
+        public void SetMusicVolume(float f)
+        {
+            MusicManager.Instance._audioSource.volume = f;
+
+            PlayerPrefs.SetFloat("MusicVol", f);
+            PlayerPrefs.Save();
+        }
+
+        public void SetSFXVolume(float f)
+        {
+            SFXManager.Instance.audioSource.volume = f;
+
+            PlayerPrefs.SetFloat("SFXVol", f);
+            PlayerPrefs.Save();
+        }
+
+        public void setTutorial(bool b)
+        {
+            Debug.Log(b.ToString());
+            PlayerPrefs.SetString("tuto", b.ToString());
+            PlayerPrefs.Save();
+        }
+    }
+}
--- Assets/Scripts/menus/configuracion.cs
+++ Assets/Scripts/menus/configuracion.cs
@@ -14,63 +14,84 @@ public class configuracion : MonoBehaviour
     public Toggle tick;
     private void Awake()
     {
-        float musicVol = PlayerPrefs.GetFloat("MusicVol", 0.3f);
-        float sfxVol = PlayerPrefs.GetFloat("SFXVol", 0.5f);
-
-        MusicManager.Instance._audioSource.volume = musicVol;
-        try
-        {
-            SFXManager.Instance.audioSource.volume = sfxVol;
-        }
-        catch
-        {
-            Debug.Log("No AudioManager encontrado");
-        }
         
-        
-        sliderMusicaOpciones.value = musicVol;
-        sliderMusicaIngame.value = musicVol;
-        sliderSFXaIngame.value = sfxVol;
-        sliderSFXOpciones.value = sfxVol;
-        if (PlayerPrefs.GetString("tuto", "True").Equals("True"))
-        {
-            tick.isOn = true;
-        }
-        else {
-            tick.isOn = false;
 
-        }
-
-
-    }
-
-    public void SetMusicVolume(float f)
-    {
-        MusicManager.Instance._audioSource.volume = f;
-     
-        PlayerPrefs.SetFloat("MusicVol", f);
-        PlayerPrefs.Save();
     }
     
-    public void SetSFXVolume(float f)
+
+    private void OnEnable()
     {
-        try
-        {
-            SFXManager.Instance.audioSource.volume = f;
-        }
-        catch
-        {
-            Debug.Log("No AudioManager encontrado");
-        }
-        
-        PlayerPrefs.SetFloat("SFXVol", f);
-        PlayerPrefs.Save();
+        // float musicVol = PlayerPrefs.GetFloat("MusicVol", 0.3f);
+        // float sfxVol = PlayerPrefs.GetFloat("SFXVol", 0.5f);
+        //
+        // try
+        // {
+        //     MusicManager.Instance._audioSource.volume = musicVol;
+        // }
+        // catch
+        // {
+        //     //It will be found eventually
+        // }
+        //
+        // try
+        // {
+        //     SFXManager.Instance.audioSource.volume = sfxVol;
+        // }
+        // catch
+        // {
+        //     Debug.Log("No AudioManager encontrado");
+        // }
+        //
+        //
+        // sliderMusicaOpciones.value = musicVol;
+        // sliderMusicaIngame.value = musicVol;
+        // sliderSFXaIngame.value = sfxVol;
+        // sliderSFXOpciones.value = sfxVol;
+        // if (PlayerPrefs.GetString("tuto", "True").Equals("True"))
+        // {
+        //     tick.isOn = true;
+        // }
+        // else
+        // {
+        //     tick.isOn = false;
+        // }
+
     }
 
-    public void setTutorial(bool b) {
-        Debug.Log(b.ToString());
-        PlayerPrefs.SetString("tuto", b.ToString());
-        PlayerPrefs.Save();
-
-    }
+    // public void SetMusicVolume(float f)
+    // {
+    //     try
+    //     {
+    //         MusicManager.Instance._audioSource.volume = f;
+    //     }
+    //     catch
+    //     {
+    //         Debug.Log("No AudioManager encontrado");
+    //     }
+    //  
+    //     PlayerPrefs.SetFloat("MusicVol", f);
+    //     PlayerPrefs.Save();
+    // }
+    //
+    // public void SetSFXVolume(float f)
+    // {
+    //     try
+    //     {
+    //         SFXManager.Instance.audioSource.volume = f;
+    //     }
+    //     catch
+    //     {
+    //         Debug.Log("No AudioManager encontrado");
+    //     }
+    //     
+    //     PlayerPrefs.SetFloat("SFXVol", f);
+    //     PlayerPrefs.Save();
+    // }
+    //
+    // public void setTutorial(bool b) {
+    //     Debug.Log(b.ToString());
+    //     PlayerPrefs.SetString("tuto", b.ToString());
+    //     PlayerPrefs.Save();
+    //
+    // }
 }
